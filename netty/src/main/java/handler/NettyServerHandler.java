07dea0fa7538a11c991ad4c90447d3824dee8ac4
fingertipy <fingertipy@126.com>
2019-10-02 16:01:40 +0800

    initizlizer netty server

---
+++ netty/src/main/java/handler/NettyServerHandler.java
@@ -0,0 +1,24 @@
+package handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 请求处理类
+ */
+public class NettyServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if (msg instanceof HttpRequest){
+            ByteBuf buf = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, buf);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, buf.readableBytes());
+            ctx.writeAndFlush(response);
+        }
+    }
+}
